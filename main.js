58bf5766e7cfd36e96680244f5252b8cfa5f1b14
Dan Reardon <dreardon1021@gmail.com>
2020-01-10 12:07:04 -0700

    Add functionality for adding/removing tasks and disable button

--- main.js
+++ main.js
@@ -1,24 +1,36 @@
+//querySelectors
 var closeButton = document.querySelector('.close-button');
 var leftTaskContainer = document.querySelector('.current-task-list');
 var addTaskButton = document.querySelector('.add-task-button');
 var taskInput = document.querySelector('.task-input');
 
-
+// EventListeners
 leftTaskContainer.addEventListener('click', closeLeftTask);
 addTaskButton.addEventListener('click', addSingleTask);
+taskInput.addEventListener('input', enableAddTaskButton);
 
+
+// Left column task functions (adding/removing/disabling)
 function closeLeftTask(event) {
   if(event.target.classList.contains('close-button')) {
     event.target.parentNode.remove();
   };
 };
 
-
 function addSingleTask() {
-  console.log('test')
   leftTaskContainer.insertAdjacentHTML('afterbegin',
   `<div class="left-task">
     <img class="close-button" src="assets/delete.svg" alt="delete">
     <p class="left-task-item">${taskInput.value}</p>
-  </div>`)
-}
+  </div>`);
+  taskInput.value = '';
+  addTaskButton.disabled = true;
+};
+
+function enableAddTaskButton() {
+  if (taskInput.value !== ''){
+    addTaskButton.disabled = false;
+  };
+};
+
+// Add Task Card Functions
